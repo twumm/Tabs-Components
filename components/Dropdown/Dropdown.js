815f08a2a010b86f05058c5e8d02a44cb03a5085
Martin Twum Mensah <martint.mensah@gmail.com>
2019-05-16 11:09:05 +0000

    feature: Complete toggleContent method. If button is clicked, toggle class of dropdown-hidden on the dropdown-content

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -18,7 +18,7 @@ class Dropdown {
   toggleContent() {
     
     // Toggle the ".dropdown-hidden" class off and on
-    this.content;
+    this.content.classList.toggle('dropdown-hidden');
   }
 }
 
